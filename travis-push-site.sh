d5db1ee2fc14e4f7d9990a13076496217ba39143
Michael Habib <1233256+MichaelHabib@users.noreply.github.com>
2021-01-25 09:16:18 +1100

    A  .ruby-version M  travis-push-site.sh

--- travis-push-site.sh
+++ travis-push-site.sh
@@ -8,8 +8,11 @@ setup_git() {
 upload() {
   cd ./_site/
   git init
+  git status
   git add -A
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+  git status
+  ls -ah
   git remote add origin https://${GH_TOKEN}@github.com/MichaelHabib/notebook-html.git > /dev/null 2>&1
   git push --quiet --force --set-upstream origin master
 }
