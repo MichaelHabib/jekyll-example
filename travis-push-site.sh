54c9b621941c13a954f54ea77d94279e2324fcf0
Michael Habib <1233256+MichaelHabib@users.noreply.github.com>
2021-01-24 15:21:36 +1100

    M  travis-push-site.sh

--- travis-push-site.sh
+++ travis-push-site.sh
@@ -5,18 +5,14 @@ setup_git() {
   git config --global user.name "Travis CI"
 }
 
-commit_website_files() {
-  cd
-  git checkout -b gh-pages
-  git add . *.html
+upload() {
+  cd ./_site/
+  git init
+  git add -A
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-}
-
-upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages gh-pages
+  git remote add origin https://${GH_TOKEN}@github.com/MichaelHabib/notebook-html.git > /dev/null 2>&1
+  git push --quiet --force --set-upstream origin master
 }
 
 setup_git
-commit_website_files
-upload_files
+upload
