c1e2ceaef0ce2ca6669c7febc8fad93eba388fa2
Michael Habib <1233256+MichaelHabib@users.noreply.github.com>
2021-01-24 15:19:47 +1100

    M  travis-push-site_test.sh

--- travis-push-site_test.sh
+++ travis-push-site_test.sh
@@ -5,19 +5,14 @@ setup_git() {
   git config --global user.name "Travis CI"
 }
 
-commit_website_files() {
+upload() {
   cd ./_site/
   git init
   git add -A
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-
-}
-
-upload_files() {
   git remote add origin https://${GH_TOKEN}@github.com/MichaelHabib/notebook-html.git > /dev/null 2>&1
   git push --quiet --force --set-upstream origin master
 }
 
 setup_git
-commit_website_files
-upload_files
+upload
